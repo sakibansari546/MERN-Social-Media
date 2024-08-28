669ce304d8eada54716025d92712f419c6b17884
Sakib Ansari <152527120+sakibansari546@users.noreply.github.com>
2024-08-28 22:34:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ dotenv.config()
 
 // Middlewares
 const corsOptions = {
-    origin: 'https://mern-social-media-tbpc.onrender.com',
+    origin: 'http://localhost:5173/',
     credentials: true,
     optionSuccessStatus: 200
 }
